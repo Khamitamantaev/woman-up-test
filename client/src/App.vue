9a476f06cd1435282ba1a5ab10d89ac49492a9df
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-11-26 20:12:46 +0500

    simple app.vue with todos array for test

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,43 @@
 <template>
-    <div>View 3 WomanUp</div>
+    <div class="todo" v-for="todo in todos" v-bind:key="todo._id">
+        <div ><strong>Todo:</strong> {{ todo.name }}</div>
+        <div ><strong>Completed:</strong><input type="checkbox"></div>
+    </div>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+           todos: [
+            {
+                "_id": 1, name: "Создать аутентификацию", done: false
+            },
+            {
+                "_id": 2, name: "Добавить crud", done: false
+            },
+            {
+                "_id": 3, name: "Сделать logout", done: false
+            }
+           ]
+        }
+    },
+    methods: {
+       
+    }
+}
 </script>
 
 <style>
+* {
+    margin: 10;
+    padding: 0;
+    box-sizing: border-box;
+}
 
+.todo {
+    padding: 15px;
+    border: 1px solid blueviolet;
+    margin-top: 5px;
+}
 </style>
